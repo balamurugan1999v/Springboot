44facf35573e673ba7090c8034f1fe143a6305ac
balamurugan1999v <31644214+balamurugan1999v@users.noreply.github.com>
2025-08-09 23:24:57 +0530

    New project

---
+++ agencies/src/main/java/com/learn/agencies/AgenciesApplication.java
@@ -0,0 +1,13 @@
+package com.learn.agencies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciesApplication.class, args);
+	}
+
+}
---
+++ agencies/src/test/java/com/learn/agencies/AgenciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.agencies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
